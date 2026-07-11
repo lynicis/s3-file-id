c3077a8f11de45a0cb56db89fb918293ffb1ee6f
Emre Sırmalı <lynicis@protonmail.com>
2026-07-11 12:17:36 +0300

    feat: export UUIDStrategy, SnowflakeStrategy and IIdStrategy from index

--- index.ts
+++ index.ts
@@ -2,10 +2,18 @@ import isValid from "./isValid";
 import decode from "./decode";
 import encode from "./encode";
 import FileId from "./fileId";
+import { UUIDStrategy } from "./strategies/UUIDStrategy";
+import { SnowflakeStrategy } from "./strategies/SnowflakeStrategy";
+import type { IIdStrategy } from "./strategies/IIdStrategy";
 
 export default {
   FileId,
   encode,
   decode,
   isValid,
+  UUIDStrategy,
+  SnowflakeStrategy,
 };
+
+export type { IIdStrategy };
+export type { SnowflakeOptions } from "./strategies/SnowflakeStrategy";
